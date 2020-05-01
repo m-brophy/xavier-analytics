5cde3ef09d4e62a6a1d31b80e51550f8a2195949
m-brophy <mbrophy@redhat.com>
2020-05-01 14:04:59 +0100

    MIGENG-444: OS Not detected

--- src/test/java/org/jboss/xavier/analytics/rules/workload/inventory/complexity/ComplexityTest.java
+++ src/test/java/org/jboss/xavier/analytics/rules/workload/inventory/complexity/ComplexityTest.java
@@ -263,7 +263,7 @@ public class ComplexityTest extends BaseTest {
         facts.put("agendaGroup", "Complexity");
 
         WorkloadInventoryReportModel workloadInventoryReportModel = new WorkloadInventoryReportModel();
-        workloadInventoryReportModel.setOsDescription("No OS Detected");
+        workloadInventoryReportModel.setOsDescription("Not detected");
         workloadInventoryReportModel.addFlagIMS(WorkloadInventoryReportModel.RDM_DISK_FLAG_NAME);
         workloadInventoryReportModel.addFlagIMS(WorkloadInventoryReportModel.SHARED_DISK_FLAG_NAME);
 
